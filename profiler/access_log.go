753ca90e8a712a6d1543b79a5b2cc702369297ec
myoan <motoki.yoan@gmail.com>
2022-07-22 22:04:05 +0900

    Enable alp

--- profiler/access_log.go
+++ profiler/access_log.go
@@ -13,6 +13,7 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/goccy/echo-tools/alp"
 	"github.com/goccy/echo-tools/gist"
 	"github.com/goccy/echo-tools/notifier"
 	"github.com/labstack/echo/v4"
@@ -27,6 +28,7 @@ type AccessLogProfiler struct {
 	echo              *echo.Echo
 	hostAddr          string
 	kataribeConfPath  string
+	alpOption         string
 	accessLogFileName string
 	botName           string
 	githubToken       string
@@ -35,9 +37,10 @@ type AccessLogProfiler struct {
 
 type AccessLogProfilerOption func(*AccessLogProfiler)
 
-func AccessLogDiscordNotifierOption(kataribeFile, botName, webhookURL, githubToken string) AccessLogProfilerOption {
+func AccessLogOption(kataribeFile, alpOption, botName, webhookURL, githubToken string) AccessLogProfilerOption {
 	return func(p *AccessLogProfiler) {
 		p.kataribeConfPath = kataribeFile
+		p.alpOption = alpOption
 		p.botName = botName
 		p.discordWebhookURL = webhookURL
 		p.githubToken = githubToken
@@ -58,6 +61,7 @@ func NewAccessLogProfiler(e *echo.Echo, hostAddr string, opts ...AccessLogProfil
 }
 
 func (p *AccessLogProfiler) Start() error {
+	log.Print("[benchmark-access-log-profiler] Start")
 	cmdMv := exec.Command(
 		"sh", "-c", fmt.Sprintf("sudo mv %s %s.`date +%%Y%%m%%d-%%H%%M%%S`", nginxAccessLog, nginxAccessLog),
 	)
@@ -77,11 +81,13 @@ func (p *AccessLogProfiler) Start() error {
 }
 
 type AccessLogRequest struct {
-	FileName          string `json:"filename"`
-	KataribeConfPath  string `json:"kataribeConfPath"`
-	BotName           string `json:"botName"`
-	GitHubToken       string `json:"githubToken"`
-	DiscordWebhookURL string `json:"discordWebhookURL"`
+	FileName          string   `json:"filename"`
+	KataribeConfPath  string   `json:"kataribeConfPath"`
+	ALPOption         string   `json:"alpOption"`
+	Routes            []string `json:"routes"`
+	BotName           string   `json:"botName"`
+	GitHubToken       string   `json:"githubToken"`
+	DiscordWebhookURL string   `json:"discordWebhookURL"`
 }
 
 func (p *AccessLogProfiler) requestURL() string {
@@ -90,9 +96,15 @@ func (p *AccessLogProfiler) requestURL() string {
 }
 
 func (p *AccessLogProfiler) Stop() error {
+	routes := make([]string, 0, len(p.echo.Routes()))
+	for _, r := range p.echo.Routes() {
+		routes = append(routes, r.Path)
+	}
 	b, err := json.Marshal(&AccessLogRequest{
 		FileName:          p.accessLogFileName,
 		KataribeConfPath:  p.kataribeConfPath,
+		ALPOption:         p.alpOption,
+		Routes:            routes,
 		BotName:           p.botName,
 		GitHubToken:       p.githubToken,
 		DiscordWebhookURL: p.discordWebhookURL,
@@ -101,7 +113,6 @@ func (p *AccessLogProfiler) Stop() error {
 		return fmt.Errorf("failed to encode access log: %w", err)
 	}
 	url := p.requestURL()
-	fmt.Printf("exec: %s\n", url)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(b))
 	if err != nil {
 		return fmt.Errorf("failed to create request: %w", err)
@@ -124,6 +135,7 @@ func (h *AccessLogHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 var (
+	alpCommandTmpl      = `sudo alp ltsv --file %s -r -m "%s" %s > %s`
 	kataribeCommandTmpl = `sudo cat %s | kataribe -conf %s > %s`
 )
 
@@ -132,9 +144,19 @@ func (h *AccessLogHandler) handle(ctx context.Context, body io.Reader) error {
 	if err := json.NewDecoder(body).Decode(&req); err != nil {
 		return fmt.Errorf("failed to decode access log request: %w", err)
 	}
+
 	if req.FileName == "" {
 		return fmt.Errorf("failed to find access-log filename")
 	}
+
+	err := execALP(ctx, req)
+	if err != nil {
+		return err
+	}
+	return execKataribe(ctx, req)
+}
+
+func execKataribe(ctx context.Context, req AccessLogRequest) error {
 	tempDir := os.TempDir()
 	kataribeFile := filepath.Join(tempDir, "kataribe.log")
 	cmd := exec.Command(
@@ -144,8 +166,8 @@ func (h *AccessLogHandler) handle(ctx context.Context, body io.Reader) error {
 	if err != nil {
 		return fmt.Errorf("failed to exec kataribe: %s: %w", string(out), err)
 	}
+	log.Print("[benchmark-access-log-profiler] send to gist")
 	if req.GitHubToken == "" || req.DiscordWebhookURL == "" {
-		log.Println("github token or discord webhook url is not found")
 		return nil
 	}
 	client, err := gist.NewClient(ctx, req.GitHubToken)
@@ -164,6 +186,43 @@ func (h *AccessLogHandler) handle(ctx context.Context, body io.Reader) error {
 	if err := discordClient.Post(&notifier.DiscordMessage{
 		Username: botName,
 		Content:  fmt.Sprintf("access-log kataribe: %s", url),
+	}); err != nil {
+		return fmt.Errorf("failed to post message to discord: %w", err)
+	}
+	return nil
+}
+
+func execALP(ctx context.Context, req AccessLogRequest) error {
+	tempDir := os.TempDir()
+	alpFile := filepath.Join(tempDir, "alp.log")
+	matchingGroups := alp.ConvertEchoRoutes(req.Routes)
+	cmd := exec.Command(
+		"sh", "-c", fmt.Sprintf(alpCommandTmpl, req.FileName, matchingGroups, req.ALPOption, alpFile),
+	)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("failed to exec alp: %s: %w", string(out), err)
+	}
+	log.Print("[benchmark-access-log-profiler] send to gist")
+	if req.GitHubToken == "" || req.DiscordWebhookURL == "" {
+		return nil
+	}
+	client, err := gist.NewClient(ctx, req.GitHubToken)
+	if err != nil {
+		return fmt.Errorf("failed to create gist client: %w", err)
+	}
+	url, err := client.UploadFile(ctx, req.FileName, alpFile)
+	if err != nil {
+		return fmt.Errorf("failed to upload access-log: %w", err)
+	}
+	botName := req.BotName
+	if botName == "" {
+		botName = "bot"
+	}
+	discordClient := notifier.NewDiscordClient(req.DiscordWebhookURL)
+	if err := discordClient.Post(&notifier.DiscordMessage{
+		Username: botName,
+		Content:  fmt.Sprintf("access-log alp: %s", url),
 	}); err != nil {
 		return fmt.Errorf("failed to post message to discord: %w", err)
 	}
